b7e66f340008bfbb8e03464983a118bb4d0f72be
ben <benjamin.slabbert@gmail.com>
2019-07-14 22:53:09 +0200

    updating script

--- build-all.sh
+++ build-all.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-VERSION=1.0.2
+VERSION=1.0.3
 
 echo "Building all Images"
 
-echo "###########################"
-echo "# Building Gateway Client #"
-echo "###########################"
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION} -f apps/gateway/client/Dockerfile .
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION} -f apps/gateway/client/Dockerfile . & \
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION} -f apps/gateway/server/Dockerfile . & \
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION} -f apps/grpc/client/Dockerfile . & \
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION} -f apps/grpc/server/Dockerfile . & \
 
-
-echo "###########################"
-echo "# Building Gateway Server #"
-echo "###########################"
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION} -f apps/gateway/server/Dockerfile .
-
-echo "########################"
-echo "# Building gRPC Client #"
-echo "########################"
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION} -f apps/grpc/client/Dockerfile .
-
-echo "########################"
-echo "# Building gRPC Server #"
-echo "########################"
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION} -f apps/grpc/server/Dockerfile .
+wait $(jobs)
 
 echo "#########################"
 echo "# Pushing to Docker hub #"
@@ -31,7 +17,9 @@ echo "#########################"
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION}
-docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION}
-docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION}
+docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION} & \
+docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION} & \
+docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION} & \
 docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION}
+
+wait $(jobs)
