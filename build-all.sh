a64d280fe093bf8fabb6e56de7afc1e558b96ce7
ben <benjamin.slabbert@gmail.com>
2019-07-14 22:55:35 +0200

    updating script

--- build-all.sh
+++ build-all.sh
@@ -4,12 +4,10 @@ VERSION=1.0.3
 
 echo "Building all Images"
 
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION} -f apps/gateway/client/Dockerfile . & \
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION} -f apps/gateway/server/Dockerfile . & \
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION} -f apps/grpc/client/Dockerfile . & \
-docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION} -f apps/grpc/server/Dockerfile . & \
-
-wait $(jobs)
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION} -f apps/gateway/client/Dockerfile .
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION} -f apps/gateway/server/Dockerfile .
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION} -f apps/grpc/client/Dockerfile .
+docker build -t benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION} -f apps/grpc/server/Dockerfile .
 
 echo "#########################"
 echo "# Pushing to Docker hub #"
@@ -17,9 +15,9 @@ echo "#########################"
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION} & \
-docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION} & \
-docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION} & \
+docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION}
+docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION}
+docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION}
 docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION}
 
 wait $(jobs)
