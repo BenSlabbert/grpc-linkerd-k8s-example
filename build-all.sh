66b0a03f82b290dfd2e1bb0d142210e328fdf1cd
ben <benjamin.slabbert@gmail.com>
2019-07-14 22:47:02 +0200

    fixing script

--- build-all.sh
+++ build-all.sh
@@ -31,7 +31,7 @@ echo "#########################"
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:$1
-docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:$1
-docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:$1
-docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:$1
+docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-client:${VERSION}
+docker push benjaminslabbert/grpc-linkerd-k8s-example-gateway-server:${VERSION}
+docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-client:${VERSION}
+docker push benjaminslabbert/grpc-linkerd-k8s-example-grpc-server:${VERSION}
